e51e51b9f2aebbbfcf50d09a33ec399be4a1dd7d
ZunOnDiscord <67102323+Finley1233445@users.noreply.github.com>
2021-11-03 19:30:53 +0000

    ZunLoginAPI
    ZunLoginAPI made by Zun#3247 on discord
    
    There Shouldn't be any issues if there is then message me on discord Zun#3247
    
    Instructions:
    
    1) Make a .cs called ZunLoginAPI
    
    2) Press CTRL + F
    
    3) Search "Token for everyone here" and replace with the token or text you want people to enter to be logged in
    
    4) Search "Discord invite here" and replace with a invite to your discord
    
    5) Go into your main code and put at the top "Using ZunLoginAPI;"
    
    6) Re-Name the key Text box to "Token"
    
    7) Then open the code to the button that you need to press too login and put And put ZunLoginAPI.Login(Token.Text);
    
    8) Last But not lease go into the ZunLoginAPI.cs again and Search for "MessageBox.Show("Logged in");" underneath this put the name of your main window and write a login functions for example
    
    Main main = new Main();
    main.Show();
    this.Hide();
    
    9) Your done enjoy

---
+++ ZunLoginAPI.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+
+namespace ZunLoginAPI
+{
+    public class Class
+    {
+        internal class Constants
+        {
+            public static string Token { get; set; }
+
+            public static string Date { get; set; }
+
+            public static string APIENCRYPTKEY { get; set; }
+
+            public static string APIENCRYPTSALT { get; set; }
+
+            public static bool Breached = false;
+
+            public static string Key = null;
+
+            public static string IV = null;
+
+            public static bool Initialized = false;
+
+            public static Random random = new Random();
+
+            public static string RandomString(int length)
+            {
+                return new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", length).Select(s => s[random.Next(s.Length)]).ToArray());
+            }
+
+            public static string HWID()
+            {
+                return WindowsIdentity.GetCurrent().User.Value;
+            }
+        }
+        public static void Login(string Token)
+        {
+            try
+            {
+                if (Token == "Token for everyone here")
+                {
+                    NameValueCollection Info = new NameValueCollection
+                    {
+                        ["token"] = Constants.Token,
+                        ["session_id"] = Constants.IV,
+                        ["api_id"] = Constants.APIENCRYPTSALT,
+                        ["api_key"] = Constants.APIENCRYPTKEY,
+                        ["hwid"] = Constants.HWID()
+                    };
+
+                    MessageBox.Show("Logged in");
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect Token, Please Go To Our Discord For the Key: Discord invite here");
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        public static void Welcome(string WelcomeLabel)
+        {
+            NameValueCollection Welc = new NameValueCollection
+            {
+                ["token"] = Constants.Token,
+                ["WelcomeLabel"] = Constants.IV,
+            };
+            WelcomeLabel = "Hello And Welcome!";
+        }
+
+        public static void exitApp()
+        {
+            NameValueCollection ExitThread = new NameValueCollection
+            {
+                ["token"] = Constants.Token,
+                ["Disabling"] = Constants.IV,
+            };
+            Environment.Exit(0);
+        }
+    }
+}
